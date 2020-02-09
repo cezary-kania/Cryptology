12279ee768c957465843086b447c7c9f7de90a9b
Cezary Kania <23578656+cezary-kania@users.noreply.github.com>
2020-02-09 14:21:45 +0100

    Add files via upload

--- ciphers/Vernam.py
+++ ciphers/Vernam.py
@@ -1,23 +1,22 @@
 def TextToBin(text):
-    return ''.join(map(lambda x: format(x,'b'),bytearray(text,'utf-8')))
-
+    #return ''.join(map(lambda x: format(x,'b'),bytearray(text,'utf-8')))
+    return bin(int.from_bytes(text.encode(),'big')).replace('b','')
 def BinToText(binN):
     n = int(binN,2)
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
 
 def Encryption(plainText, key):
-    pTasBytes = TextToBin(plainText)
-    keyAsBytes = TextToBin(key)
-    return BinToText(''.join(map(str,list(map(lambda a,b: (int(a) + int(b)) % 2,pTasBytes,keyAsBytes)))))
+    binPlainText = TextToBin(plainText)
+    binKey = TextToBin(key)
+    return ''.join(list(map(str,list(map(lambda a,b: (int(a) + int(b)) % 2,binPlainText,binKey)))))
 
 def Decryption(cipherText, key):
-    cTasBytes = TextToBin(cipherText)
-    keyAsBytes = TextToBin(key)
-    return BinToText(''.join(map(str,list(map(lambda a,b: (int(a) + int(b)) % 2,cTasBytes,keyAsBytes)))))
+    binKey = TextToBin(key)
+    return BinToText(''.join(list(map(str,list(map(lambda a,b: (int(a) + int(b)) % 2,cipherText,binKey))))))
 
 def Main():
-    plainText = 'abc'
-    key = 'abx'
+    plainText = input('Input text to encode:')
+    key = input('Input secret key:')
     cipherText = Encryption(plainText,key)
     print(cipherText)
     plainText = Decryption(cipherText,key)
